c55195374f8e975e99489aaf0146bd3287506e9f
Alberto <alberto.godoybor@gmail.com>
2023-10-20 01:16:16 +0200

    Version 1.2

--- server/download.php
+++ server/download.php
@@ -1,5 +1,10 @@
 <?php
 
-if (isset($_POST[""])){
+// Vamos a realizar la descargar del CV
+$ruta_pdf = "CV.pdf";
+$nombreSalida="CV Alberto Godoy Borrego.pdf";
 
-} // End if de la llamada al boton 
+// Definimos las cabecera para descargar en pdf
+header("Content-type: application/pdf");
+header('Content-Disposition:attachment;filename="'.$nombreSalida.'"');
+readfile($ruta_pdf);
